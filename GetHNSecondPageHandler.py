91c9a684853e9ecde90708a6ef5818cf46649714
Gleb Popov <glebche@gmail.com>
2011-10-16 21:11:18 -0400

    fixed more link. added news2 controller

---
+++ GetHNSecondPageHandler.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+#
+# Hacker News Droid API: returns homepage news in JSON or XML using HTML Parser
+# Gleb Popov. September 2011
+#
+
+import os
+import re
+import logging
+import datetime
+import time
+from UserString import MutableString
+from django.utils import simplejson
+from google.appengine.ext import webapp 
+from google.appengine.ext import db
+from google.appengine.ext.webapp import util
+import Formatter
+import AppConfig
+import GAHelper
+from xml.sax.saxutils import escape
+import APIUtils
+import GAHelper
+
+class HackerNewsSecondPageHandler(webapp.RequestHandler):
+	
+	#controller main entry		
+	def get(self,format='json',page=''):
+		#set content-type
+		self.response.headers['Content-Type'] = Formatter.contentType(format)
+		
+		referer = ''
+		if ('HTTP_REFERER' in os.environ):
+			referer = os.environ['HTTP_REFERER']
+		
+		returnData = MutableString()
+		returnData = APIUtils.getHackerNewsSecondPageContent(page,format,self.request.url, referer, self.request.remote_addr)
+		if (not returnData or returnData == None or returnData == '' or returnData == 'None'):
+			#call the service again this time without the pageID
+			returnData = APIUtils.getHackerNewsPageContent('',format,self.request.url, referer, self.request.remote_addr)
+			
+		#track this request
+		GAHelper.trackGARequests('/news2', self.request.remote_addr, referer)
+		
+		if (not returnData):
+			returnData = ''
+		
+		#output to the browser
+		self.response.out.write(Formatter.dataWrapper(format, returnData))
